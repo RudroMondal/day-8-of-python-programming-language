54b93c7b345dfbe4c2821da971a721a7319ab17e
RudroMondal <rudromondal151@gmail.com>
2026-02-07 00:38:53 +0600

    Dictionary project
    Some Dictionary project

---
+++ Aecess_Dictionary_Key.py
@@ -0,0 +1,13 @@
+# Creat a dictionary
+vehicles ={}
+
+#Add 'key' and 'value' in dictionary
+vehicles["Car"]="black"
+vehicles["Bike"]="red"
+vehicles["Auto"]="yellow"
+
+# Aecess specific key
+color=vehicles["Bike"]
+
+# print vehicles
+print(color)
